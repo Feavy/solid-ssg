b31aed572e3f702683d82ee636fb792820117724
Feavy <quentin071100@live.fr>
2022-05-28 22:35:02 +0200

    Add doctype html

--- ssg/scripts/writeToDisk.js
+++ ssg/scripts/writeToDisk.js
@@ -6,7 +6,7 @@ import server from "../lib/index.js";
 async function write() {
   const res = await server({ url: process.argv[4] });
   fs.mkdir(path.dirname(process.argv[3]), {recursive: true},  () =>
-    fs.writeFile(process.argv[3], res, () => process.exit(0))
+    fs.writeFile(process.argv[3], "<!DOCTYPE html>\n"+res, () => process.exit(0))
   );
 }
 write();
